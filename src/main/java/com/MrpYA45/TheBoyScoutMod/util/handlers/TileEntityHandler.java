46753bc35c4a32f68d483ab36b9230e6a13c4fca
Pablo Fdez <pablofdezbravo@gmail.com>
2019-01-20 13:44:48 +0100

    Update TileEntityHandler.java
    Potentially Dangeorous alternative prefix

--- src/main/java/com/MrpYA45/TheBoyScoutMod/util/handlers/TileEntityHandler.java
+++ src/main/java/com/MrpYA45/TheBoyScoutMod/util/handlers/TileEntityHandler.java
@@ -6,17 +6,18 @@ import com.MrpYA45.TheBoyScoutMod.blocks.containers.gold_box.GoldBoxTileEntity;
 import com.MrpYA45.TheBoyScoutMod.blocks.containers.iron_box.IronBoxTileEntity;
 import com.MrpYA45.TheBoyScoutMod.blocks.containers.paper_bin.PaperBinTileEntity;
 import com.MrpYA45.TheBoyScoutMod.blocks.containers.wooden_box.WoodenBoxTileEntity;
+import com.MrpYA45.TheBoyScoutMod.util.Reference;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class TileEntityHandler {
 	public static void registerTileEntity() {
-		GameRegistry.registerTileEntity(WoodenBoxTileEntity.class, "wooden_box");
-		GameRegistry.registerTileEntity(IronBoxTileEntity.class, "iron_box");
-		GameRegistry.registerTileEntity(GoldBoxTileEntity.class, "gold_box");
-		GameRegistry.registerTileEntity(DiamondBoxTileEntity.class, "diamond_box");
-		GameRegistry.registerTileEntity(EmeraldBoxTileEntity.class, "emerald_box");
-		GameRegistry.registerTileEntity(PaperBinTileEntity.class, "paper_bin");
+		GameRegistry.registerTileEntity(WoodenBoxTileEntity.class, Reference.MOD_ID + ":wooden_box");
+		GameRegistry.registerTileEntity(IronBoxTileEntity.class, Reference.MOD_ID + ":iron_box");
+		GameRegistry.registerTileEntity(GoldBoxTileEntity.class, Reference.MOD_ID + ":gold_box");
+		GameRegistry.registerTileEntity(DiamondBoxTileEntity.class, Reference.MOD_ID + ":diamond_box");
+		GameRegistry.registerTileEntity(EmeraldBoxTileEntity.class, Reference.MOD_ID + ":emerald_box");
+		GameRegistry.registerTileEntity(PaperBinTileEntity.class, Reference.MOD_ID + ":paper_bin");
 	}
 
 }
